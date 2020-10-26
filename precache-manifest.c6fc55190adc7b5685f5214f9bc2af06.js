9b6a8282f41f9cb1f2cf8894bd9705875687216e
Sofia Flores <71674784+SofiaFloresMtz@users.noreply.github.com>
2020-10-26 15:52:24 -0700

    Updates

---
+++ precache-manifest.c6fc55190adc7b5685f5214f9bc2af06.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72edc02c6db04bfad972248efdc8252d",
+    "url": "/Practica1React/index.html"
+  },
+  {
+    "revision": "b8f728655b746ac31044",
+    "url": "/Practica1React/static/css/main.705aeb9d.chunk.css"
+  },
+  {
+    "revision": "55ba321b4fb028ac4b57",
+    "url": "/Practica1React/static/js/2.18c4e933.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Practica1React/static/js/2.18c4e933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8f728655b746ac31044",
+    "url": "/Practica1React/static/js/main.7656f14b.chunk.js"
+  },
+  {
+    "revision": "e2b691ba5a5e9c89e2c6",
+    "url": "/Practica1React/static/js/runtime-main.5d167055.js"
+  },
+  {
+    "revision": "99b3fdc91230a39cb5ff26454da9866e",
+    "url": "/Practica1React/static/media/logoEscuela.99b3fdc9.PNG"
+  }
+]);
